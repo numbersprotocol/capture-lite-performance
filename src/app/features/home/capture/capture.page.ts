41bef778ea5b0f65648bc639311702b4c2178b36
Sean Wu <seanwu1105@gmail.com>
2020-12-24 12:27:28 +0800

    Implement logout.

--- src/app/features/home/capture/capture.page.ts
+++ src/app/features/home/capture/capture.page.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-capture',
   templateUrl: './capture.page.html',
   styleUrls: ['./capture.page.scss'],
 })
-export class CapturePage implements OnInit {
+export class CapturePage {
   constructor() {}
-
-  ngOnInit() {}
 }
--- src/app/features/home/profile/profile.page.ts
+++ src/app/features/home/profile/profile.page.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { defer } from 'rxjs';
+import { concatMapTo } from 'rxjs/operators';
+import { BlockingAction } from '../../../shared/services/blocking-action/blocking-action.service';
+import { DiaBackendAuthService } from '../../../shared/services/dia-backend/auth/dia-backend-auth.service';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage {
-  constructor() {}
+  constructor(
+    private readonly diaBackendAuthService: DiaBackendAuthService,
+    private readonly blockingAction: BlockingAction,
+    private readonly router: Router
+  ) {}
+
+  logout() {
+    const action$ = this.diaBackendAuthService
+      .logout$()
+      .pipe(concatMapTo(defer(() => this.router.navigate(['/login']))));
+
+    this.blockingAction.run$(action$).pipe(untilDestroyed(this)).subscribe();
+  }
 }
